f84ebf88c07893ed6b2cb87105fdbe7603d3594f
Cezar Contantino Cardoso Moreira <cezarcardosomoreira@gmail.com>
2023-02-08 21:00:52 -0300

    aula8

---
+++ PYTHON3/aula3.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Cezar Moreira')
+print(1, 'Cezar "Moreira"')
+
+# Aspas duplas
+print("Cezar Moreira")
+print(2, "Cezar 'Moreira'")
+
+# Escape
+print("Cezar \"Moreira\"")
+
+# r
+print(r"Cezar \"Moreira\"")
