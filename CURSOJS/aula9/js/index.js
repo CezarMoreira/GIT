196facac1f0b5676ba7370e6379c54080299e7ef
Cezar Contantino Cardoso Moreira <cezarcardosomoreira@gmail.com>
2023-02-24 22:50:34 -0300

    js

---
+++ CURSOJS/aula9/js/index.js
@@ -0,0 +1,14 @@
+// alert('Alerta Cezar');
+
+
+// digitar window no console do navegador mostra vairios comandos que
+// é possivel utilizar 
+
+let num1 = prompt('digite um numero');
+let num2 = prompt('digite um numero');
+let Num1 = Number(num1);
+let Num2 = Number(num2);
+let resultado = Num1 + Num2;
+alert(` o resultado foi, ${resultado}`);
+
+
